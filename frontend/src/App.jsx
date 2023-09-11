ea702d8dc760ff812d33898b1554447da38b612d
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2023-09-11 20:31:14 +0530

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
-import {Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Homepage from '../../components/Homepage';
 import Login from '../../components/Login';
 import Register from '../../components/Register';
 import Profile from '../../components/Profile';
 
-  
+
 function App() {
   return (
     <div className="App">
