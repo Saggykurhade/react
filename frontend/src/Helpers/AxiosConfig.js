0cc9441605878f59fc225b3c31ba5388c78332d2
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2024-01-31 18:32:52 +0530

    updates

--- frontend/src/Helpers/AxiosConfig.js
+++ frontend/src/Helpers/AxiosConfig.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-var BackendUrl = "http://localhost:8000/api/v1"
-if (process.env.MODE == "PRODUCTION") {
-    BackendUrl = "https://awdiz-backend-lfuw.onrender.com/api/v1"
-}
+// var BackendUrl = "http://localhost:8000/api/v1"
+// if (process.env.MODE == "PRODUCTION") {
+   var BackendUrl = "https://awdiz-backend-lfuw.onrender.com/api/v1"
+// }
 
 const api = axios.create({ baseURL: BackendUrl })
 
